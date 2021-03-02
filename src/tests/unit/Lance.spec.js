c7c01ca06f5783c4d6a49a8e1b005d34b058345c
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-01 22:58:45 -0300

    descrevendo casos de testes

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -18,3 +18,13 @@ test('Emite um lance quando o valor é maior que zero', () => {
     const lancesEmitidos = wrapper.emitted('novo-lance')
     expect(lancesEmitidos).toHaveLength(1)
 })
+
+test('Emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
+})
